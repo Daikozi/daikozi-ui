e0c2ea725c625d36273cb364d41d26fa068b3ed7
Daikozi <daikozi@gmail.com>
2022-01-08 21:49:58 +0100

    Fix css prop

--- src/components/Typography/Typography.styles.ts
+++ src/components/Typography/Typography.styles.ts
@@ -1,5 +1,12 @@
-import { css, jsx } from "@emotion/react";
+import { css, SerializedStyles } from "@emotion/react";
+import { h1, h2 } from "../../theme/typography";
 
-export const typography = css({
-  color: "green",
-});
+export const typography = (variant: string): SerializedStyles => {
+  switch (variant) {
+    case "h1":
+      css({ ...h1 });
+      break;
+    default:
+      css({ ...h2 });
+  }
+};
--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import { FC } from "react";
 import { TypographyProps } from "./Typography.propTypes";
 import * as styles from "./Typography.styles";
@@ -9,36 +10,40 @@ export const Typography: FC<TypographyProps> = ({
 }) => {
   switch (component || variant) {
     case "h1":
-      return <h1 css={styles.typography}>{children}</h1>;
+      return <h1 css={styles.typography(component || variant)}>{children}</h1>;
     case "h2":
-      return <h2 css={styles.typography}>{children}</h2>;
+      return <h2 css={styles.typography(component || variant)}>{children}</h2>;
     case "h3":
-      return <h3 css={styles.typography}>{children}</h3>;
+      return <h3 css={styles.typography(component || variant)}>{children}</h3>;
     case "h4":
-      return <h4 css={styles.typography}>{children}</h4>;
+      return <h4 css={styles.typography(component || variant)}>{children}</h4>;
     case "h5":
-      return <h5 css={styles.typography}>{children}</h5>;
+      return <h5 css={styles.typography(component || variant)}>{children}</h5>;
     case "h6":
-      return <h6 css={styles.typography}>{children}</h6>;
+      return <h6 css={styles.typography(component || variant)}>{children}</h6>;
     case "body2":
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
     case "button":
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
     case "caption":
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
     case "inherit":
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
     case "overline":
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
     case "subtitle1":
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
     case "subtitle2":
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
     case "div":
-      return <div css={styles.typography}>{children}</div>;
+      return (
+        <div css={styles.typography(component || variant)}>{children}</div>
+      );
     case "span":
-      return <span css={styles.typography}>{children}</span>;
+      return (
+        <span css={styles.typography(component || variant)}>{children}</span>
+      );
     default:
-      return <p css={styles.typography}>{children}</p>;
+      return <p css={styles.typography(component || variant)}>{children}</p>;
   }
 };
